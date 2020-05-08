a2188125f6227b653bdcea1250e403f8cabb027e
MartaB32 <58725514+MartaB32@users.noreply.github.com>
2020-05-08 17:49:55 +0200

    added drums pianoroll

--- pianoroll/script.js
+++ pianoroll/script.js
@@ -10,8 +10,20 @@ function sample_play(sample,timeout) {
 
 var dupa = '';
 
-function onButtonClick(character) {
-	dupa = dupa + character;
+function onButtonClickA() {
+	dupa = dupa + 'A';
+	updateDisplayElement();
+}
+function onButtonClickB() {
+	dupa = dupa + 'B';
+	updateDisplayElement();
+}
+function onButtonClickC() {
+	dupa = dupa + 'C';
+	updateDisplayElement();
+}
+function onButtonClickEmpty() {
+	dupa = dupa + ' ';
 	updateDisplayElement();
 }
 
